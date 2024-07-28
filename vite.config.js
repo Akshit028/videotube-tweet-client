e7521b58b63014e2d8045337f3bd2ae648de0fcb
Akshit Solanki <solankiakii28@gmail.com>
2024-07-28 17:41:10 +0530

    proxy

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // server : {
-  //   proxy : {
-  //     '/api' : 'http://localhost:5000'
-  //   }
-  // }
+  server : {
+    proxy : {
+      '/api' : 'https://mern-proj-124k.onrender.com'
+    }
+  }
 })
