54a9334bae0d00f518ddc5960e6d8a9572032cd4
Akshit Solanki <solankiakii28@gmail.com>
2024-07-31 21:30:55 +0530

    ui changes 1

--- src/components/Loading/LoadingSpinner.jsx
+++ src/components/Loading/LoadingSpinner.jsx
@@ -4,9 +4,11 @@ import Logo from "../Logo";
 function LoadingSpinner() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
-      <div className="flex items-center">
+      <div className="flex gap-5 items-center">
         <Logo />
-        <div className="w-12 h-12 ml-4 border-4 border-t-transparent border-b-transparent border-white rounded-full animate-spin"></div>
+        <div class="flex items-center justify-center ">
+          <div class="w-6 h-6 border-b-2 border-white rounded-full animate-spin"></div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -9,29 +9,29 @@ import {
 
 const Support = () => {
   const personalInfo = {
-    name: "H Harinadh",
-    email: "eccentricharry@gmail.com",
+    name: "Akshit Solanki",
+    email: "solankiakii28@gmail.com",
   };
   const links = [
-    { name: "Twitter", icon: FaTwitter, url: "https://x.com/Hari_5297" },
+    { name: "Twitter", icon: FaTwitter, url: "" },
     {
       name: "LinkedIn",
       icon: FaLinkedin,
-      url: "https://www.linkedin.com/in/h-harinadh/",
+      url: "https://www.linkedin.com/in/akshit-solanki-64b825239/",
     },
-    { name: "GitHub", icon: FaGithub, url: "https://github.com/Eccentric-Harry" },
+    { name: "GitHub", icon: FaGithub, url: "https://github.com/Akshit028" },
     {
       name: "Discord",
       icon: FaDiscord,
-      url: "https://discord.com/users/eccentricharry",
+      url: "/",
     },
   ];
 
   return (
     <section className="w-full  flex justify-center items-center">
-      <div className="bg-gray-900 text-white p-8 rounded-lg shadow-lg max-w-lg mx-auto">
+      <div className="bg-[#212121] text-white p-8 rounded-lg shadow-lg max-w-lg mx-auto">
         <div className="flex flex-col items-center mb-6">
-          <div className="bg-blue-600 p-4 rounded-full mb-4">
+          <div className="bg-red-700 p-4 rounded-full mb-4">
             <FaQuestionCircle className="text-4xl" />
           </div>
 
@@ -41,7 +41,7 @@ const Support = () => {
           <h4 className="text-2xl font-bold text-center mb-2 ">
             {personalInfo.name}
           </h4>
-          <p className="text-blue-400 text-lg mb-4">{personalInfo.email}</p>
+          <p className="text-white text-lg mb-4">{personalInfo.email}</p>
         </div>
         <div className="space-y-4">
           {links.map((link) => (
@@ -50,9 +50,9 @@ const Support = () => {
               href={link.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center p-3 bg-gray-800 rounded-lg hover:bg-blue-700 transition duration-300"
+              className="flex items-center p-3 bg-[#0E0E0E] rounded-lg hover:bg-red-700 transition duration-300"
             >
-              <link.icon className="text-blue-400 mr-3 text-xl" />
+              <link.icon className="text-white mr-3 text-xl" />
               <span className="text-lg">{link.name}</span>
             </a>
           ))}
